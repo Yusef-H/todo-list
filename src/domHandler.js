873eb10af82a58a6c4cd86370dfcf318ea68194c
Yusef-H <yusefhalabi456@gmail.com>
2023-04-17 10:01:28 +0300

    build project skeleton with a collapsible sidebar

---
+++ src/domHandler.js
@@ -0,0 +1,7 @@
+
+const sidebarButton = document.querySelector('.sidebar-collapse-button');
+const sidebar = document.querySelector('.sidebar');
+
+sidebarButton.addEventListener('click', () => {
+  sidebar.classList.toggle('sidebar--show');
+});
