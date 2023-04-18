489587020fc3b362f69d68518c7905619283e540
Yusef-H <yusefhalabi456@gmail.com>
2023-04-18 21:28:38 +0300

    add todo in ui with checkbox, title and date

--- src/domHandler.js
+++ src/domHandler.js
@@ -1,7 +1,7 @@
-
+import { format} from 'date-fns'
 const sidebarButton = document.querySelector('.sidebar-collapse-button');
 const sidebar = document.querySelector('.sidebar');
-const todosListContainer = document.querySelector('.todos-list');
+const todoListContainer = document.querySelector('.todo-list-container');
 
 sidebarButton.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar-show');
@@ -17,7 +17,30 @@ function displayProjectTodos(project){
 
 function displayTodo(todo){
     const todoContainer = document.createElement('div');
-    todosListContainer.appendChild(todoContainer);
+    const leftDiv = document.createElement('div');
+    const rightDiv = document.createElement('div');
+
+    const checkBox = document.createElement("input");
+    const title = document.createElement("h3");
+    const date = document.createElement("h3");
+    
+
+    checkBox.setAttribute("type", "checkbox");
+    leftDiv.appendChild(checkBox);
+
+    title.innerHTML = todo.title;
+    leftDiv.appendChild(title);
+
+    
+    date.innerHTML = format(todo.dueDate, "yyyy/MM/dd");
+    rightDiv.appendChild(date);
+
+    todoContainer.appendChild(leftDiv);
+    todoContainer.appendChild(rightDiv);
+
+
+    todoContainer.classList.add('todo-container');
+    todoListContainer.appendChild(todoContainer);
 }
 
 
--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ import {displayProjectTodos} from './domHandler.js';
 
 const exProject = Project("Example");
 const todo1 = TodoFactory("example1", "ex1", new Date(2014, 1, 11));
+const todo2 = TodoFactory("example2", "ex2", new Date(2014, 1, 11));
 exProject.addTodo(todo1);
+exProject.addTodo(todo2);
 displayProjectTodos(exProject);
